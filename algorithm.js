bcd4bf428b82b6ad180a22a738b1c06bb16fb1a4
Aniels <96707275+Aniels@users.noreply.github.com>
2022-10-12 23:00:36 +0100

    find the algrothm that can list all solution

--- algorithm.js
+++ algorithm.js
@@ -1,44 +1,60 @@
-let queenPlacedList = [ [3,1]];
-let n = 5;
-let ansArry;
+function find_all_arrangements(n) {
+    const allRes = [];
 
-function generatetable(n) {
-    let table = []
-    for(let i = 0; i < n; i++) {
-        table[i] = new Array(n).fill(true);
+    function isValid(row, col, board) {
+  // Checks the ← direction
+    for(var i=0; i<col; i++){
+      if (board[row][i] === "q") {
+        return false;
+      }
     }
-    return  table;
-}
 
-let table = generatetable(n)
-console.log(table);
+    // Checks the ↖ direction 
+    for(var i=row, j=col; i>=0 && j>=0; i--, j--){
+      if (board[i][j] === "q") {
+        return false;
+      }
+    }
 
-/*
-function algorithm(position){
-    console.log(position);
-}
-*/
+    // Checks the ↙ direction 
+    for(var i=row, j=col; j>=0 && i<n; i++, j--){
+      if (board[i][j] === "q") {
+        return false;
+      }
+    }
 
+    return true;
+    };
 
-function placeQueen(table, postionList){
-    for(let i = 0; i < postionList.length; i ++){
-        let placement = postionList[i]
-        table[placement[1]][placement[0]] = false;
-        for(let index = 0; index < n; index ++){
-            table[placement[1]][index] = false;
-            table[index][placement[0]] = false;
-            
-            if(placement[1] + index < n ){
-            table[placement[1] + index][placement[0] + index] = false;
-            table[placement[1] + index][placement[0] - index] = false;
-
-            table[placement[1] - index][placement[0] + index] = false;
-            table[placement[1] - index][placement[0] + index] = false;
+    function find(col, result) {
+        if (col === n) {
+          // this deep clone the 2d array
+          allRes.push(JSON.parse(JSON.stringify(result)));
+          return;
+        }
+        for (let i = 0; i < n; i++) {
+            if (isValid(i, col, result)) {
+                result[i][col] = "q";
+                find(col + 1, result)
+                result[i][col] = "-";
             }
-            
         }
     }
+
+  function generateBoard(n){
+    var board=[];
+    for(var i=0; i<n; i++){
+      board[i]=[];
+      for(var j=0; j<n; j++){
+        board[i][j]="-";
+      }
+    }
+    return board;
+  }
+
+  var board = generateBoard(n);
+  find(0, board);
+  return allRes;
 }
 
-console.log(table);
-placeQueen(table, queenPlacedList)
+console.log(find_all_arrangements(5))
